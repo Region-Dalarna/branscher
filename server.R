ffb6661d127cbe89d4fac6f1d96a9db2edb717a1
Peter Möller <peter.moller@regiondalarna.se>
2026-08-26 15:59:10 +0200

    Initiera Shinyapp-projekt

---
+++ server.R
@@ -0,0 +1,9 @@
+shinyServer(function(input, output, session) {
+
+  telemetri_server(telemetry, navigation_id = 'flikval', forsta_flik = 'Tab 1')
+
+  output$example_text <- renderText({{
+    'Byt ut detta mot din egen serverlogik.'
+  }})
+
+})
